26c46b0d3365366dfd2eab215d55a6d2f9de3858
GiovaneCaruso <88150171+giovanecarusoredacao@users.noreply.github.com>
2024-01-23 19:30:23 -0300

    Create br_ibge_censo_2022.area_territorial_densidade_demografica_municipio.sql

---
+++ br_ibge_censo_2022.area_territorial_densidade_demografica_municipio.sql
@@ -0,0 +1,45 @@
+## Consultas criadas para divulgar os dados do Censo 2022 na Base dos Dados! Confira o conteúdo por aqui: https://x.com/basedosdados/status/1749842569792528728?s=20
+
+
+## Qual o Município com menor população no Brasil?
+SELECT
+  m.nome AS nome_municipio,
+  c.*
+FROM
+  `basedosdados.br_ibge_censo_2022.area_territorial_densidade_demografica_municipio` AS c
+JOIN
+  `basedosdados.br_bd_diretorios_brasil.municipio` AS m
+ON
+  c.id_municipio = m.id_municipio
+ORDER BY
+  c.populacao_residente 
+LIMIT 1;
+
+## Qual o Município com mario população no Brasil?
+
+SELECT
+  m.nome AS nome_municipio,
+  c.*
+FROM
+  `basedosdados.br_ibge_censo_2022.area_territorial_densidade_demografica_municipio` AS c
+JOIN
+  `basedosdados.br_bd_diretorios_brasil.municipio` AS m
+ON
+  c.id_municipio = m.id_municipio
+ORDER BY
+  c.populacao_residente DESC
+LIMIT 1;
+
+## Maior Área de Unidade Territorial
+SELECT
+  m.nome AS nome_municipio,
+  c.*
+FROM
+  `basedosdados.br_ibge_censo_2022.area_territorial_densidade_demografica_municipio` AS c
+JOIN
+  `basedosdados.br_bd_diretorios_brasil.municipio` AS m
+ON
+  c.id_municipio = m.id_municipio
+ORDER BY
+  c.area_unidade_territorial DESC
+LIMIT 1;
